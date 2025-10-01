78bea042cd9ffb2d6d7da225a467548096b4f932
zinine73 <zinine@naver.com>
2025-10-01 15:49:30 +0900

    GameOver

--- Assets/Scripts/DestroyOutOfBounds.cs
+++ Assets/Scripts/DestroyOutOfBounds.cs
@@ -2,24 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 3. UnityEvent 이용
+public class GameOverAction : UnityEngine.Events.UnityEvent { }
 public class DestroyOutOfBounds : MonoBehaviour
 {
+    // 1. SpawnManager를 정의, 연결하는 방법
+    //SpawnManager sm;
+    // 2. delegate 이용
+    // public delegate void GameOverHandler();
+    // public static event GameOverHandler OnGameOver;
+    // 3.
+    public static GameOverAction OnGameOver = new GameOverAction();
     float topBound = 30.0f;
     float lowerBound = -10.0f;
 
+    void Start()
+    {
+        // SpawnManager 연결
+        // sm = GameObject.Find("SpawnManager").
+        //     GetComponent<SpawnManager>();
+        // sm = GameObject.FindGameObjectWithTag("SM").
+        //     GetComponent<SpawnManager>();
+    }
+
     void Update()
     {
-        // if (transform.position.z > topBound)
-        // {
-        //     Destroy(gameObject);
-        // }
-        // else if (transform.position.z < lowerBound)
-        // {
-        //     Destroy(gameObject);
-        // }
-        if (transform.position.z > topBound ||
-            transform.position.z < lowerBound)
+        if (transform.position.z > topBound)
         {
+            Destroy(gameObject);
+        }
+        else if (transform.position.z < lowerBound)
+        {
+            //Debug.Log("Game Over!");
+            // SpawnManager의 DisplayGameOver를 호출
+            //sm.DisplayGameOver();
+            //OnGameOver();
+            OnGameOver.Invoke();
             Destroy(gameObject);
         }
     }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,21 +5,47 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
+    [SerializeField] GameObject gameOverPanel;
+
     float spawnRangeX = 20.0f;
     float spawnPosZ = 20.0f;
+    float startDelay = 2.0f;
+    float spawnInterval = 1.5f;
 
-    void Update()
+    // void OnEnable()
+    // {
+    //     DestroyOutOfBounds.OnGameOver += DisplayGameOver;
+    // }
+
+    // void OnDisable()
+    // {
+    //     DestroyOutOfBounds.OnGameOver -= DisplayGameOver;
+    // }
+
+    void Start()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Vector3 spawnPos = new Vector3(
-                Random.Range(-spawnRangeX, spawnRangeX),
-                0,
-                spawnPosZ);
-            Instantiate(animalPrefabs[animalIndex],
-                spawnPos,
-                animalPrefabs[animalIndex].transform.rotation);
-        }    
+        DestroyOutOfBounds.OnGameOver.AddListener(DisplayGameOver);
+        gameOverPanel.SetActive(false);
+        InvokeRepeating(
+            "SpawnRandomAnimal",
+            startDelay,
+            spawnInterval);
+    }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Vector3 spawnPos = new Vector3(
+            Random.Range(-spawnRangeX, spawnRangeX),
+            0,
+            spawnPosZ);
+        Instantiate(animalPrefabs[animalIndex],
+            spawnPos,
+            animalPrefabs[animalIndex].transform.rotation);
+    }
+
+    public void DisplayGameOver()
+    {
+        gameOverPanel.SetActive(true);
     }
 }
